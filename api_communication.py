f5ccf1d841838bae4f20555d43f334ebdd1930a2
Raymond Su <raymondsu@Raymonds-MacBook-Pro.local>
2024-09-10 01:20:57 -0700

    download first episode successfully

--- api_communication.py
+++ api_communication.py
@@ -1,9 +1,51 @@
-import feedparser
 from pathlib import Path
 import requests
-import openai
-import whisper
+# import openai
+# import whisper
+from dotenv import load_dotenv
+import os
+import json
 
+load_dotenv()
+
+X_ListenAPI_Key = os.getenv('X_ListenAPI_Key')
+listennotes_episode_endpoint = 'https://listen-api.listennotes.com/api/v2/episodes'
+headers_listennotes = {
+  'X-ListenAPI-Key': X_ListenAPI_Key,
+}
+WSB_id = 'f59182551ad149b693d7856390ea6f7a'
+episode_id = 'f399406330bb4d82972c1f5743480993'
+
+def get_episode_audio_url(episode_id):
+    url = listennotes_episode_endpoint + '/' + episode_id
+    response = requests.request('GET', url, headers=headers_listennotes)
+
+    data = response.json()
+
+    episode_title = data['title']
+    thumbnail = data['thumbnail']
+    podcast_title = data['podcast']['title']
+    audio_url = data['audio']
+
+    return audio_url, thumbnail, podcast_title, episode_title
+
+audio_url, thumbnail, podcast_title, episode_title = get_episode_audio_url(episode_id)
+filename = "podcast_episode.mp3"
+folder_path = "./podcast"
+# Create the podcast folder if it doesn't exist
+if not os.path.exists(folder_path):
+    os.makedirs(folder_path)
+file_path = os.path.join(folder_path, filename)
+
+response = requests.get(audio_url, stream=True)
+with open(file_path, 'wb') as file:
+    for chunk in response.iter_content(chunk_size=1024):
+        if chunk:
+            file.write(chunk)
+
+
+
+"""
 
 def get_transcribe_podcast(rss_url, local_path):
     print ("Starting Podcast Transcription Function")
@@ -33,3 +75,4 @@ def get_transcribe_podcast(rss_url, local_path):
         for chunk in r.iter_content(chunk_size=8192):
             f.write(chunk)
 
+"""
