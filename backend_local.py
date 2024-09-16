6542f405e2bb3146523f857dc7497c78b413fba4
Raymond Su <raymondsu@Raymonds-MacBook-Pro.local>
2024-09-15 17:51:31 -0700

    rename file name

--- backend_local.py
+++ backend_local.py
@@ -6,14 +6,16 @@ from openai import OpenAI
 from dotenv import load_dotenv
 import os
 from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
 import datetime
 import certifi
+from pathlib import Path
 
 load_dotenv()
 
 X_ListenAPI_Key = os.getenv('X_ListenAPI_Key')
 MONGODB_PWD = os.getenv('MONGODB_PWD')
-mongoDB_url = f"mongodb+srv://raymondjsu:{MONGODB_PWD}@cluster0.uile4.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+mongoDB_uri = f"mongodb+srv://raymondjsu:{MONGODB_PWD}@cluster0.uile4.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 API_KEY_chatGPT = os.getenv('API_KEY_chatGPT')
 
 client = OpenAI(
@@ -21,12 +23,6 @@ client = OpenAI(
 )
 
 podcast_feed_url = "https://www.spreaker.com/show/5725002/episodes/feed"
-# listennotes_episode_endpoint = 'https://listen-api.listennotes.com/api/v2/episodes'
-# headers_listennotes = {
-#   'X-ListenAPI-Key': X_ListenAPI_Key,
-# }
-# WSB_id = 'f59182551ad149b693d7856390ea6f7a'
-# episode_id = 'f399406330bb4d82972c1f5743480993'
 
 
 def get_episode_audio_url(podcast_feed_url):
@@ -44,20 +40,36 @@ def get_episode_audio_url(podcast_feed_url):
     publish_date = podcast_feed.entries[0].published
     audio_url = podcast_feed.entries[0].links[1].href
 
+    print(f"Audio URL: {audio_url}")
+
     return audio_url, episode_title, publish_date
 
 def transcribe(audio_data):
     model = whisper.load_model("base.en")
-    transcript = model.transcribe(audio_data)
+    transcript = model.transcribe(audio_data, fp16=False)
+
+    print(transcript["text"][:100])
 
     return transcript["text"]
 
 def generate_summary(transcript):
     instructPrompt = """
-                follow a process that distills the following content into key bullet points, focusing on major news items, economic data, stock market reactions, and any additional insights provided. Here's a basic structure: 1. identify the main topics. 2. Group related information. 3. highlight key numbers and reactions. 4. condense for clarity. 5. maintain a logical flow. Now please create a brief yet comprehensive summary that conveys the essential information:
+                follow a process that distills the following content into key bullet points, focusing on major news items, economic data, stock market reactions, and any additional insights provided. 
+                Here's a basic structure: 
+                1. identify the main topics. 
+                2. Group related information. 
+                3. highlight key numbers and reactions. 
+                4. condense for clarity. 
+                5. maintain a logical flow. 
+                
+                Now please create a brief yet comprehensive summary that conveys the essential information:
 """
     TLDRPrompt = """
- please provide a TLDR version of the following transcript, please limit to 30 to 60 words, or about 1-3 sentences. The goal is to provide a quick, high-level overview that captures the essence of the content. Here's the transcript:
+                please provide a TLDR version of the following transcript, 
+                please limit to 30 to 60 words, or about 1-3 sentences. 
+                The goal is to provide a quick, high-level overview that captures the essence of the content. 
+                
+                Here's the transcript:
 """
 
     prompt = TLDRPrompt + transcript
@@ -77,8 +89,8 @@ def generate_summary(transcript):
 
     return TLDROutput, SummaryOutput
 
-def store_episode_data(url, title,TLDROutput, SummaryOutput, publish_date):
-    client = MongoClient(url, tlsCAFile=certifi.where())
+def store_episode_data(uri, title,TLDROutput, SummaryOutput, publish_date):
+    client = MongoClient(uri, tlsCAFile=certifi.where())
     db = client.podcast_summarizer
     episodes = db.episodes
     episode_data = {
@@ -91,32 +103,43 @@ def store_episode_data(url, title,TLDROutput, SummaryOutput, publish_date):
     episodes.insert_one(episode_data)
     print("Stored episode data in MongoDB")
 
+def ping():
+
+    mongoDB_uri = f"mongodb+srv://raymondjsu:{MONGODB_PWD}@cluster0.uile4.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+
+    client = MongoClient(mongoDB_uri, server_api=ServerApi("1"))
+    try:
+        client.admin.command("ping")
+        print("Pinged your deployment. You successfully connected to MongoDB!")
+    except Exception as e:
+        print(e)
+
 def main():
     print("Downloading podcast episode...")
-    audio_url, episode_title, publish_date = get_episode_audio_url(podcast_feed_url)
-    filename = f"{episode_title}.mp3"
-    folder_path = "./podcast"
+    episode_url, episode_title, publish_date = get_episode_audio_url(podcast_feed_url)
+    episode_file = f"{episode_title.replace(' ', '_')}.mp3"
+    folder_path = os.path.abspath("./podcast/")
 
     # Create the podcast folder if it doesn't exist
-    if not os.path.exists(folder_path):
-        os.makedirs(folder_path)
-    file_path = os.path.join(folder_path, filename)
+    Path(folder_path).mkdir(parents=True, exist_ok=True)
+    # Absolute path for the downloaded file
+    episode_path = os.path.join(folder_path, episode_file)
 
-    # Download the podcast episode as mp3
-    # response = requests.get(audio_url, stream=True)
-    # with open(file_path, 'wb') as file:
-    #     for chunk in response.iter_content(chunk_size=1024):
-    #         if chunk:
-    #             file.write(chunk)
-    print("Downloade complete!")
+    # # Download the podcast episode as mp3
+    with requests.get(episode_url, stream=True) as r:
+        r.raise_for_status()
+        with open(episode_path, 'wb') as f:
+            for chunk in r.iter_content(chunk_size=8192):
+                f.write(chunk)
 
+    print(f"Downloaded episode to: {episode_path}")
     print("Transcribing podcast episode...")
-    transcript = transcribe(file_path)
+    transcript = transcribe(episode_path)
     print("Transcription complete!")
-    print("Generating summary...")
-    TLDROutput, SummaryOutput = generate_summary(transcript)
-    print("Summary complete!")
-    store_episode_data(mongoDB_url, episode_title, TLDROutput, SummaryOutput, publish_date)
+    # print("Generating summary...")
+    # # TLDROutput, SummaryOutput = generate_summary(transcript)
+    # print("Summary complete!")
+    # store_episode_data(mongoDB_uri, episode_title, TLDROutput, SummaryOutput, publish_date)
     # Delete the audio file
     # if os.path.exists(file_path):
     #     os.remove(file_path)
@@ -127,3 +150,4 @@ def main():
 # will need to rewrite to def save_transcript(episode_id):
 if __name__ == '__main__':
     main()
+    
